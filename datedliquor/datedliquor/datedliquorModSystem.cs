4e19c19ae39ac4599c3e7024b7198a9facfff3a4
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-11-29 21:32:08 -0600

    Add project files.

---
+++ datedliquor/datedliquor/datedliquorModSystem.cs
@@ -0,0 +1,35 @@
+﻿using datedliquor.src.Behaviors;
+using datedliquor.src.Harmony;
+using HarmonyLib;
+using System.Reflection;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace datedliquor
+{
+    public class datedliquorModSystem : ModSystem
+    {
+        Harmony harmonyInstance;
+        public override void Start(ICoreAPI api)
+        {
+            var modid = Mod.Info.ModID;
+            harmonyInstance = new Harmony(modid);
+
+            api.RegisterCollectibleBehaviorClass(modid+":AlcoholInfo", typeof(CollectibleBehaviorAlcoholInfo));
+
+            //harmonyInstance.Patch(typeof(Vintagestory.GameContent.BlockEntityCondenser).GetMethod("TryPut", BindingFlags.NonPublic | BindingFlags.Instance),
+              // postfix: new HarmonyMethod(typeof(harmPatches).GetMethod("Postfix_BlockEntityAnvil_TryPut")));
+
+            harmonyInstance.Patch(typeof(Vintagestory.GameContent.BlockEntityCondenser).GetMethod("TryTake", BindingFlags
+                .NonPublic | BindingFlags.Instance),
+                prefix: new HarmonyMethod(typeof(harmPatches).GetMethod("Prefix_BlockEntityAnvil_TryTake")));
+        }
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            harmonyInstance = new Harmony(Mod.Info.ModID);
+            harmonyInstance.Patch(typeof(CollectibleObject).GetMethod("GetHeldItemInfo"),
+                postfix: new HarmonyMethod(typeof(harmPatches).GetMethod("Postfix_GetHeldItemInfo")));
+
+        }
+    }
+}
---
+++ datedliquor/datedliquor/src/Behaviors/CollectibleBehaviorAlcoholInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+using Cairo;
+using Newtonsoft.Json.Linq;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+
+namespace datedliquor.src.Behaviors
+{
+    public class CollectibleBehaviorAlcoholInfo : CollectibleBehavior
+    {
+        private ICoreAPI Api;
+
+        public ExtraHandbookSection[] ExtraHandBookSections;
+
+        public CollectibleBehaviorAlcoholInfo(CollectibleObject collObj)
+        : base(collObj)
+        {
+        }
+        public override void OnLoaded(ICoreAPI api)
+        {
+            Api = api;
+            JsonObject jsonObject = collObj.Attributes?["handbook"]?["extraSections"];
+            if (jsonObject != null && jsonObject.Exists)
+            {
+                ExtraHandBookSections = jsonObject?.AsObject<ExtraHandbookSection[]>();
+            }
+        }
+
+        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+            dsc.Append("Alcohol Info Behavior Test Text");
+        }
+        
+        public override void OnCreatedByCrafting(ItemSlot[] allInputslots, ItemSlot outputSlot, GridRecipe byRecipe, ref EnumHandling bhHandling)
+        {
+            
+            base.OnCreatedByCrafting(allInputslots, outputSlot, byRecipe, ref bhHandling);
+        }
+
+        public override ItemStack OnTransitionNow(ItemSlot slot, TransitionableProperties props, ref EnumHandling handling)
+        {
+            return base.OnTransitionNow(slot, props, ref handling);
+        }
+    }
+}
