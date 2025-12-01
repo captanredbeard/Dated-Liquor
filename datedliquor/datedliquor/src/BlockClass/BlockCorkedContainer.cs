369d4da106c36ff235d1bdf9cc470931e5553006
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-12-01 15:20:37 -0600

    main corked container class

---
+++ datedliquor/datedliquor/src/BlockClass/BlockCorkedContainer.cs
@@ -0,0 +1,14 @@
+﻿using ACulinaryArtillery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datedliquor.src.BlockClass
+{
+    public class BlockCorkedContainer : BlockBottle
+    {
+
+    }
+}
