d5219ef371a2960ec1d05010181d6f60e3ceb311
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-12-02 23:29:06 -0600

    add rough plan to ModSystem

--- datedliquor/datedliquor/datedliquorModSystem.cs
+++ datedliquor/datedliquor/datedliquorModSystem.cs
@@ -10,11 +10,31 @@ namespace datedliquor
 {
     public class datedliquorModSystem : ModSystem
     {
+        /*
+         * Setup basic yaml config via configlib or autoconfiglib
+         * https://mods.vintagestory.at/configlib
+         * https://mods.vintagestory.at/autoconfiglib
+         *
+         * 
+         * Consider whether to rewrite ACA's BlockBottle to make our bottles a bit easier to code.
+         * 
+         * Look at transitionable properties for use in the dating system and get it displaying properly.
+         * 
+         * add cork/uncork methods for recipes and other interactions to call when bottles are corked/uncorked.
+         * 
+         * make sure to leave room for the future recipe system implementation.
+         * 
+         * 
+         * 
+         * 
+        */
         Harmony harmonyInstance;
         public override void Start(ICoreAPI api)
         {
             var modid = Mod.Info.ModID;
-            api.RegisterBlockClass(modid + ":TallRackBottle", typeof(BlockTallRackBottle));
+            api.RegisterBlockClass(modid + ":CorkedContainer", typeof(BlockCorkedContainer));
+
+
             harmonyInstance = new Harmony(modid);
             //harmonyInstance.PatchAll();
         }
